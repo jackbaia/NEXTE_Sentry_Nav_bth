6802af910d135c1135b59c374cd4e9d43c1484a8
Bob <101231181+jackbaia@users.noreply.github.com>
2025-07-16 10:24:54 +0800

    删除了一些无用文件

--- takeoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#floder=`ls ~/231021/log  -l| grep ^-|wc -l`
-#sh ~/231021/nvidiajingjian/Fast-Drone-250_bkp/shfiles/pix_r.sh ~/231021/log/$floder.bag
-
-rostopic pub -1  /px4ctrl/takeoff_land quadrotor_msgs/TakeoffLand "takeoff_land_cmd: 1"
